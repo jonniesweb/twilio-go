56dd90d7d73aecbccb74381c3a6025ff33e98ad4
Jon Simpson <jon.simpson@shopify.com>
2020-03-26 23:59:55 -0400

    flow

--- studio_flow.go
+++ studio_flow.go
@@ -11,44 +11,40 @@ type FlowService struct {
 	client *Client
 }
 
-type EasyFlowType = map[string]interface{}
+type Flow struct {
+	Sid          string     `json:"sid"`
+	AccountSid   string     `json:"account_sid"`
+	FriendlyName string     `json:"friendly_name"`
+	Status       string     `json:"status"`
+	Valid        string     `json:"valid"`
+	Errors       string     `json:"errors"`
+	WebhookUrl   string     `json:"webhook_url"`
+	DateCreated  TwilioTime `json:"date_created"`
+	DateUpdated  TwilioTime `json:"date_updated"`
+	Url          string     `json:"url"`
+	Definition   string     `json:"definition"`
+}
 
-// TODO: This should really map to the correct types, but theres some nested
-// JSON that Twilio responds with that isn't worth the trouble.
+func (f *FlowService) Create(ctx context.Context, data url.Values) (*Flow, error) {
+  flow := new(Flow)
 
-// type Flow struct {
-// 	Sid          string     `json:"sid"`
-// 	FriendlyName string     `json:"friendly_name"`
-// 	Status       string     `json:status""`
-// 	Valid        string     `json:"valid"`
-// 	Errors       string     `json:"errors"`
-// 	WebhookUrl   string     `json:"webhook_url"`
-// 	DateCreated  TwilioTime `json:"date_created"`
-// 	DateUpdated  TwilioTime `json:"date_updated"`
-// 	Url          string     `json:"url"`
-// 	// Definition   map[string]string `json:"definition"`
-// }
-
-func (f *FlowService) Create(ctx context.Context, data url.Values) (EasyFlowType, error) {
-	var flow EasyFlowType
-
-	err := f.client.CreateResource(ctx, FlowPathPart, data, &flow)
+	err := f.client.CreateResource(ctx, FlowPathPart, data, flow)
 
 	return flow, err
 }
 
-func (f *FlowService) Get(ctx context.Context, sid string) (EasyFlowType, error) {
-	var flow EasyFlowType
+func (f *FlowService) Get(ctx context.Context, sid string) (*Flow, error) {
+  flow := new(Flow)
 
-	err := f.client.GetResource(ctx, FlowPathPart, sid, &flow)
+	err := f.client.GetResource(ctx, FlowPathPart, sid, flow)
 
 	return flow, err
 }
 
-func (f *FlowService) Update(ctx context.Context, sid string, data url.Values) (EasyFlowType, error) {
-	var flow EasyFlowType
+func (f *FlowService) Update(ctx context.Context, sid string, data url.Values) (*Flow, error) {
+  flow := new(Flow)
 
-	err := f.client.UpdateResource(ctx, FlowPathPart, sid, data, &flow)
+	err := f.client.UpdateResource(ctx, FlowPathPart, sid, data, flow)
 
 	return flow, err
 }
