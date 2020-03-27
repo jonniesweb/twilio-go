ca74bd9a4a8fe78c19af5bee7136dab3cb264207
Elliot Dohm <elliot.dohm@shopify.com>
2020-03-26 21:26:40 -0400

    Finish flows

--- studio_flow.go
+++ studio_flow.go
@@ -30,3 +30,23 @@ func (f *FlowService) Create(ctx context.Context, data url.Values) (*Flow, error
 
 	return flow, err
 }
+
+func (f *FlowService) Get(ctx context.Context, sid string) (*Flow, error) {
+	flow := new(Flow)
+
+	err := f.client.GetResource(ctx, FlowPathPart, sid, flow)
+
+	return flow, err
+}
+
+func (f *FlowService) Update(ctx context.Context, sid string, data url.Values) (*Flow, error) {
+	flow := new(Flow)
+
+	err := f.client.UpdateResource(ctx, FlowPathPart, sid, data, flow)
+
+	return flow, err
+}
+
+func (f *FlowService) Delete(ctx context.Context, sid string) error {
+	return f.client.DeleteResource(ctx, FlowPathPart, sid)
+}
